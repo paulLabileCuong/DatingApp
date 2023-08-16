9c9366b84c4ec6e16aab39d04ccdb792e6d4772e
Le Cuong <cuongkien91@gmail.com>
2023-08-16 14:58:57 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,9 @@ namespace API
 
             app.UseHttpsRedirection();
 
+            app.UseCors(x =>
+                x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -61,5 +66,6 @@ namespace API
                 endpoints.MapControllers();
             });
         }
+
     }
 }
