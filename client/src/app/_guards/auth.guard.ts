27df90749e716799d844e6ea4d5c19b06c85e6a9
Le Cuong <cuongkien91@gmail.com>
2023-09-05 20:38:57 +0700

    End of section 6

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'  // <- ADD THIS
+})
+
+export class AuthGuard implements CanActivate {
+
+  constructor (private accountService: AccountService, private toastr: ToastrService) {}
+ 
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map( user => {
+        if (user) { 
+          return true;
+        }else
+          this.toastr.error('You shall not pass!')
+          return false;
+      })    
+    )
+  }
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit {
   
   model : any = {} //empty object
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr : ToastrService) { }
   
   ngOnInit(): void {
   }
@@ -21,17 +23,16 @@ export class NavComponent implements OnInit {
   login() {
     // Gọi phương thức login từ service AccountService và đăng ký để lắng nghe kết quả
     this.accountService.login(this.model).subscribe(response => {
-      // Được gọi khi yêu cầu đăng nhập thành công
-      console.log(response); // In ra thông tin phản hồi từ yêu cầu đăng nhập
+      this.router.navigateByUrl('/members'); // Chuyển hướng đến trang members
     }, error => {
       // Được gọi khi yêu cầu đăng nhập gặp lỗi
       console.log(error); // In ra thông tin lỗi từ yêu cầu đăng nhập
+      this.toastr.error(error.error); // Hiển thị thông báo lỗi
     });
   }
 
   logout() {
     this.accountService.logout();
-    console.log('logged out');
+    this.router.navigateByUrl('/');
   }
-
 }
